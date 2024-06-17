87fc6c16f945edfd39e77d87df4c034ddd9defc3
Glazelf <35176230+Glazelf@users.noreply.github.com>
2024-06-17 07:45:28 +0200

    Formatting and fix content
    [2:] should make it pop the first two items in the array off. Then joins them back together with a space.
    Haven't tested this though teehee

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+spaceString = " "
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-
-
 
 @client.event
 async def on_message(message):
@@ -17,18 +16,18 @@ async def on_message(message):
         return
 
     if message.content.startswith('$servers'):
-         str1 = ""
-         for guild in client.guilds:
-            str1 = str1 + guild.name + ": " + str(guild.id) + "\n"
+        serverListString = ""
+        for guild in client.guilds:
+            serverListString = serverListString + guild.name + ": " + str(guild.id) + "\n"
 
-         await message.channel.send(str1)
+        await message.channel.send(serverListString)
 
     if message.content.startswith('$channels'):
-        serverID = message.content.split(" ")[1]
+        serverID = message.content.split(spaceString)[1]
         guild = client.get_guild(int(serverID))
         strChannels = "" + guild.name + "\n"
         for channel in guild.channels:
-          strChannels = strChannels + channel.name + "\n"
+            strChannels = strChannels + channel.name + "\n"
 
         strChannels = strChannels[0:2000]
         await message.channel.send(strChannels)
@@ -36,13 +35,11 @@ async def on_message(message):
     channelID = ""
     content = ""
     if message.content.startswith("$send"):
-        channelID = message.content.split(" ")[1]
-        content = message.content.split(" ")[2]
+        channelID = message.content.split(spaceString)[1]
+        content = spaceString.join(message.content.split(spaceString)[2:])
         print(channelID)
         print(content)
         channel = client.get_channel(int(channelID))
         await channel.send(content)
-    
 
-  
 client.run('add ur own token here silly')
