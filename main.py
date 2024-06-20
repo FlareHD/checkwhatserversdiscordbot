dc0bbe2973dab4f01452a4b49129c0e417e46627
FlareHD <60042359+FlareHD@users.noreply.github.com>
2024-06-21 00:02:43 +0100

    New commands and added comments.

--- main.py
+++ main.py
@@ -17,18 +17,19 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+#Displays total list of servers the bot is in with server IDs:
     if message.content.startswith('$servers'):
          serverListString = ""
          for guild in client.guilds:
             serverListString = serverListString + guild.name + ": " + str(guild.id) + "\n"
          await message.channel.send(serverListString)
-
+# Displays the channels in a specified server:
     if message.content.startswith('$channels'):
         serverID = message.content.split(spaceString)[1]
         guild = client.get_guild(int(serverID))
         strChannels = "" + guild.name + "\n"
         printIDs = " " + guild.name + "\n"
+#PrintIDs logs the channel name and ID to the console; whereas only sending channel names on discord:
         for channel in guild.channels:
           strChannels = strChannels + channel.name + "\n" 
           printIDs = printIDs + channel.name + " " + str(channel.id) + "\n"
@@ -36,19 +37,37 @@ async def on_message(message):
         strChannels = strChannels[0:2000]
         await message.channel.send(strChannels)
         print(printIDs)
-
+#Send a message to a discord server channel:
     channelID = ""
     content = ""
     if message.content.startswith("$send"):
-        channelID = message.content.split(" ")[1]
-        content = message.content.split(" ")[2]
         channelID = message.content.split(spaceString)[1]
         content = spaceString.join(message.content.split(spaceString)[2:])
         print(channelID)
         print(content)
         channel = client.get_channel(int(channelID))
         await channel.send(content)
+#Send users a bot DM:
+    userID = ""
+    dmcontent = ""
+    if message.content.startswith("$dm"):
+        userID = message.content.split(spaceString)[1]
+        dmcontent = spaceString.join(message.content.split(spaceString)[2:])
+        print(userID)
+        print(dmcontent)
+        user = await client.fetch_user(userID)
+        await user.send(dmcontent)
+        await message.channel.send
+# Shows DM content to bot:
+    if message.guild is None and not message.author.bot:
+        channel = client.get_channel(int(1251944380114141244))
+        await channel.send(f'DM from {message.author}: {message.content}')
+# Leave a specified discord server:
+    if message.content.startswith("$leave"):
+        serverID = message.content.split(spaceString)[1]
+        guild = client.get_guild(int(serverID))
+        await guild.leave()
 
-
+  
   
 client.run('nonono upload ur own token')
